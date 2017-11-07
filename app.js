e99d87f209d6e7ff847fe6a569326d5410bee9fb
Tim LaTorre <timlatorre@gmail.com>
2017-11-07 12:15:30 -0500

    through Nunjucks

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const nunjucks = require('nunjucks');
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views');
 
 const PORT = 3000;
 app.listen(PORT, () => {
@@ -8,13 +13,13 @@ app.listen(PORT, () => {
 
 
 app.use(function (req, res, next) {
- 
     console.log(req.method, req.url);
     next();
   })
 
 
-app.get('/', (req,res)=>{
-    res.send('hello');
-   
+app.get('/index.html', (req,res)=>{
+    const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+    res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
+
